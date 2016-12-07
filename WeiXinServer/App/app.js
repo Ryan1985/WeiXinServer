3e3f2c39bdcec994fa9f3b873ccc90810a053353
xieyuliang <xieyuliang1985@126.com>
2016-12-07 14:13:12 +0800

    [mod]addMysql

--- WeiXinServer/App/app.js
+++ WeiXinServer/App/app.js
@@ -10,5 +10,16 @@
 
         // 3rd Party Modules
         
-    ]);
+    ]).config(['$locationProvider', function ($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }]).controller('appController', ['$scope', '$location','$http', function($scope, $location,$http) {
+        var openId = $location.search().openId;
+        $http.get('/api/User?paramsString=' + JSON.stringify({ 'OpenId': openId }))
+            .success(function(response) {
+                $scope.userList = response;
+            });
+    }]);
 })();
