49ae40472286cfa19d3c217588bbd76d0af2455e
xieyuliang <xieyuliang1985@126.com>
2016-12-08 17:13:48 +0800

    [mod]完成订单添加修改

--- WeiXinServer/App/app.js
+++ WeiXinServer/App/app.js
@@ -16,10 +16,45 @@
             requireBase: false
         });
     }]).controller('appController', ['$scope', '$location','$http', function($scope, $location,$http) {
-        var openId = $location.search().openId;
-        $http.get('/api/User?paramsString=' + JSON.stringify({ 'OpenId': openId }))
-            .success(function(response) {
-                $scope.userList = response;
+
+
+        $scope.viewModel = {
+            DepartureTime: new Date(),
+            Departure:'上海南站'
+        };
+
+
+        $scope.refresh = function() {
+            $http.get('/api/Order?OpenId=' + $location.search().openid)
+                .success(function(response) {
+                    if (response.HasError == true) {
+                        alert(response.ErrorMessage);
+                    } else {
+                        if (response.QueryResult.Id != 0) {
+                            $scope.viewModel = response.QueryResult;
+                            $scope.viewModel.DepartureTime = new Date($scope.viewModel.DepartureTime);
+                        }
+                    }
+                });
+        };
+
+
+        $scope.Save = function () {
+            $scope.viewModel.OpenId = $location.search().openid;
+            $http.post("/api/Order", $scope.viewModel)
+            .success(function (response) {
+                if (response.HasError == true) {
+                    alert(response.ErrorMessage);
+                } else {
+                    alert('保存成功');
+                    $scope.refresh();
+                }
+            })
+            .error(function (data) {
+                alert(data);
             });
+        };
+
+        $scope.refresh();
     }]);
 })();
